4f5736f1646902dca27128f6673db7137bc3077f
yomZsamora <samaurah@gmail.com>
2024-09-04 17:14:55 +0300

    Created serializer for user registration.

---
+++ app/users/serializers/user_registration_serializer.py
@@ -0,0 +1,9 @@
+from users.models import User
+from rest_framework import serializers
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password')
+        
