9ce187b7ce2dfc60b9f2528e21dd15b1624194b0
yomZsamora <samaurah@gmail.com>
2024-09-04 15:59:01 +0300

    Created User model.

--- app/core/settings.py
+++ app/core/settings.py
@@ -32,23 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -85,25 +75,6 @@ DATABASES = {
         'PORT': os.getenv("MYSQL_PORT", "3306")
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 
 # Internationalization
---
+++ app/users/models/user.py
@@ -0,0 +1,38 @@
+import pytz
+from datetime import datetime
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+class User(models.Model):
+    email = models.EmailField(max_length=100, null=False, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    password = models.CharField(max_length=255)
+    last_login = models.DateTimeField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    _password = None
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self._password = raw_password
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def refresh_last_login(self):
+        self.last_login = datetime.now(tz=pytz.timezone('TIME_ZONE'))
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.password = make_password(self.password)
+        super(User, self).save(*args, **kwargs)
