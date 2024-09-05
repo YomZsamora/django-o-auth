a4dcf3fb5e5bd9846cec30748f3b27226a84b733
yomZsamora <samaurah@gmail.com>
2024-09-05 12:59:34 +0300

    Configured URL for authenticating via OAuth.

--- app/auth/urls.py
+++ app/auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from auth.views import OAuth2CallbackView
+from auth.views import OAuth2CallbackView, OAuthLoginView
 
 urlpatterns = [
     path('oauth2callback', OAuth2CallbackView.as_view(), name='oauth-callback'),
+    path("oauth-login", OAuthLoginView.as_view(), name="oauth-login"),
 ]
