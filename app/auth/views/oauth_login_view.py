1d4d238535b50d269655f0b097b435355f327e59
yomZsamora <samaurah@gmail.com>
2024-09-05 13:24:11 +0300

    Ensured we're updated login time once user is successfully authenticated.

--- app/auth/views/oauth_login_view.py
+++ app/auth/views/oauth_login_view.py
@@ -1,6 +1,8 @@
 from utils.views import ApiResponse
 from utils.google import verify_and_decode_id_token
 
+from users.helpers import get_user_by_email
+
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,6 +18,8 @@ class OAuthLoginView(APIView):
         
         token_info = verify_and_decode_id_token(id_token_str)
         user_email = token_info.get('email')
+        user = get_user_by_email(user_email)
+        user.refresh_last_login()
         
         api_response = ApiResponse()
         api_response.message = "Logged in successfully."
