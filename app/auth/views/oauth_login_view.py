3a4295066e04cfe4b992cb99d911e92c661c8c79
yomZsamora <samaurah@gmail.com>
2024-09-05 13:00:43 +0300

    Incorporated token verification in OAuthLoginView using google-auth.

--- app/auth/views/oauth_login_view.py
+++ app/auth/views/oauth_login_view.py
@@ -1,10 +1,27 @@
+from utils.views import ApiResponse
+from utils.google import verify_and_decode_id_token
+
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 
 class OAuthLoginView(APIView):
     
     def post(self, request: Request) -> Response:
         
-        pass
+        id_token_str = request.data['id_token']
+        if not id_token_str:
+            raise AuthenticationFailed()
+        
+        token_info = verify_and_decode_id_token(id_token_str)
+        
+        api_response = ApiResponse()
+        api_response.message = "Logged in successfully."
+        api_response.data = token_info
+        
+        return Response(vars(api_response))
+        
+
+            
         
