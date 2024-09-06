6b5039705cf35891f56d11099fb3e3eb85cceba1
yomZsamora <samaurah@gmail.com>
2024-09-06 20:13:33 +0300

    Added seeding for registered user for testing.

--- app/tests/abstract_api_test.py
+++ app/tests/abstract_api_test.py
@@ -1,8 +1,17 @@
 from faker import Faker
+from users.models import User
 from rest_framework.test import APITestCase, URLPatternsTestCase
 
 class AbstractAPITest(APITestCase, URLPatternsTestCase):
     
     def setUp(self) -> None:
         self.fake = Faker()
+        
+    def seed_registered_user(self):
+        self.registered_user = User.objects.create(
+            email=self.fake.email(),
+            first_name=self.fake.first_name(),
+            last_name=self.fake.last_name(),
+            password=self.fake.password()
+        )
     
