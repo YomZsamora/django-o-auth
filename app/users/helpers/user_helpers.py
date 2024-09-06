322e906cbe4c2230e2b6c450f08f326012a1e5b2
yomZsamora <samaurah@gmail.com>
2024-09-06 10:13:58 +0300

    Implemented helper function for fetching user given the email.

--- app/users/helpers/user_helpers.py
+++ app/users/helpers/user_helpers.py
@@ -3,6 +3,13 @@ from utils.exceptions.custom_exceptions import DoesNotExist
 
 def get_user_by_email(email: str) -> User:
     
+    """
+    Retrieve a User object based on the provided email address.
+    Parameters: email (str): The email address of the user to retrieve.
+    Returns: User: The User object associated with the provided email address.
+    Raises: DoesNotExist: Custom exception raised when the user with the given email does not exist in the database.
+    """
+    
     try:
         return User.objects.get(email=email)
     except (User.DoesNotExist):
