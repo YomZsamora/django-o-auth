ad73386313e6e45b8368dde47dbf456eedefe0d8
yomZsamora <samaurah@gmail.com>
2024-09-06 20:02:16 +0300

    Updated returned response after successful user registration.

--- app/users/views/user_registration_view.py
+++ app/users/views/user_registration_view.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+
+from utils.views import ApiResponse
+
 from users.serializers import UserRegistrationSerializer
 
 class UserRegisterationView(generics.CreateAPIView):
@@ -9,6 +13,24 @@ class UserRegisterationView(generics.CreateAPIView):
     """
     
     serializer_class = UserRegistrationSerializer
+    
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        
+        api_response = ApiResponse()
+        api_response.code = status.HTTP_201_CREATED
+        api_response.message = f"{serializer.instance.email} has been registered successfully."
+        api_response.data = {
+            "id": serializer.instance.id,
+            "email": serializer.instance.email,
+            "first_name": serializer.instance.first_name,
+            "last_name": serializer.instance.last_name
+        }
+
+        return Response(vars(api_response), api_response.code)
 
     def perform_create(self, serializer):
         
