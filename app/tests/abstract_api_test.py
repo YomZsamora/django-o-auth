0418711f4e95637fbddcf079678b31957039e95e
yomZsamora <samaurah@gmail.com>
2024-09-06 17:44:56 +0300

    Initialized Faker in AbstractAPITest.

--- app/tests/abstract_api_test.py
+++ app/tests/abstract_api_test.py
@@ -1,4 +1,8 @@
+from faker import Faker
 from rest_framework.test import APITestCase, URLPatternsTestCase
 
 class AbstractAPITest(APITestCase, URLPatternsTestCase):
+    
+    def setUp(self) -> None:
+        self.fake = Faker()
     
