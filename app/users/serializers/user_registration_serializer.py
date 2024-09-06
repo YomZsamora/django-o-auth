42d678f2032963390f66dd115370633cb14afde8
yomZsamora <samaurah@gmail.com>
2024-09-06 10:14:32 +0300

    Implemented serializer class for user registration.

--- app/users/serializers/user_registration_serializer.py
+++ app/users/serializers/user_registration_serializer.py
@@ -3,6 +3,13 @@ from rest_framework import serializers
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     
+    """
+    Serializer class for user registration.
+    Attributes:
+    - model (User): The User model to serialize data from.
+    - fields (tuple): Fields to include in the serialization process.
+    """
+    
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'password')
